bb7a0468c6369e2476879271bc4e25748bfee7c5
jlgaoyuan <18904420062@189.cn>
2018-12-18 23:35:01 +0800

    new

--- sc/src/main/java/javaframe/sc/ScApplication.java
+++ sc/src/main/java/javaframe/sc/ScApplication.java
@@ -8,6 +8,7 @@ public class ScApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ScApplication.class, args);
+        System.out.println("hello");
     }
 
 }
