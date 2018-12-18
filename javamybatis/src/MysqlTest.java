4af4fce6b2bf89e5c95e2efb2c4b55d77f375dac
jlgaoyuan <18904420062@189.cn>
2018-12-18 22:35:26 +0800

    new

--- javamybatis/src/MysqlTest.java
+++ javamybatis/src/MysqlTest.java
@@ -1,26 +1,35 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 
 public class MysqlTest {
-    public static void main( String[] args ) {
-        try{
+    public static void main(String[] args) {
+        Connection conn = null;
+        Statement stmt = null;
+        try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             //2.连接到数据"库"上去
-            Connection conn= DriverManager.getConnection("jdbc:mysql://10.255.255.224:3306/mysql?characterEncoding=utf-8", "root", "123");
+            conn = DriverManager.getConnection("jdbc:mysql://10.255.255.224:3306/javadb?characterEncoding=utf-8", "root", "123");
             //3.构建SQL命令
-            Statement stmt=conn.createStatement();
-            stmt.execute("use mysql");
-            try (ResultSet rs = stmt.executeQuery("SELECT  * from user")) {
-                //position result to first
-                rs.first();
-                System.out.println(rs.getString(2)); //result is "Hello World!"
+            stmt = conn.createStatement();
+            stmt.execute("use javadb");
+            ResultSet rs = stmt.executeQuery("SELECT  * from tb_users");
+            //position result to first
+            rs.first();
+            System.out.println(rs.getString(2)); //result is "Hello World!"
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                stmt.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
             }
 
 
-        }catch (Exception e){
-            e.printStackTrace();
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
