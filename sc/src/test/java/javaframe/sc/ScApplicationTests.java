8d1edf6de27db649c48c33daf466c830a11d7b69
jlgaoyuan <18904420062@189.cn>
2018-12-18 23:17:45 +0800

    new

---
+++ sc/src/test/java/javaframe/sc/ScApplicationTests.java
@@ -0,0 +1,17 @@
+package javaframe.sc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ScApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
---
+++ sp/src/test/java/javaframe/sp/SpApplicationTests.java
@@ -0,0 +1,17 @@
+package javaframe.sp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
